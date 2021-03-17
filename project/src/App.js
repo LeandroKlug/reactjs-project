fd5e8cbf9bd76b0288eaa773782dc34e9098487d
LeandroKlug <leandro.softnetwork@gmail.com>
2021-03-17 17:49:00 -0300

    consume de API dataFetching

--- project/src/App.js
+++ project/src/App.js
@@ -5,60 +5,55 @@ class App extends Component {
 
   state = {
     counter: 0,
-    posts: [
-      {
-        id: 1,
-        title: 'O titulo 1',
-        body: 'O corpo 1'
-      },
-      {
-        id: 2,
-        title: 'O titulo 2',
-        body: 'O corpo 2'
-      },
-      {
-        id: 3,
-        title: 'O titulo 3',
-        body: 'O corpo 3'
-      }
-    ]
+    posts: []
   };
-  timeoutUpdate = null;
-
+ 
   componentDidMount(){
-    this.handleTimeout();
+    this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+
+    const photoResponse = fetch('https://jsonplaceholder.typicode.com/photos')
+
+
+    const [posts, photos] = await Promise.all([postsResponse, photoResponse]);
+
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url }
+    });
+
+    this.setState({posts: postsAndPhotos});
+
   }
 
   componentDidUpdate(){
-    this.handleTimeout();
   }
 
   componentWillUnmount(){
-    clearTimeout(this.timeoutUpdate);
-  }
-
-  handleTimeout = () => {
-    const { posts, counter } = this.state;
-    posts[0].title = 'O titulo mudou';
-
-    this.timeoutUpdate = setTimeout(() => {
-      this.setState({ posts, counter: counter + 1 });
-    }, 1000);
   }
     
   render(){
-    const { posts, counter } = this.state;
+    const { posts } = this.state;
 
     return (
-      <div className="App">
-        <h2>{counter}</h2>
-        {posts.map(post => (
-          <div key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.body}</p>
-          </div>
-        ))}
-      </div>
+      <section className="container">
+        <div className="posts">
+          {posts.map(post => (
+            <div className="post">
+              <img src={post.cover} alt={post.title}/>
+              <div key={post.id} className="post-content">
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     );
   }
 }
