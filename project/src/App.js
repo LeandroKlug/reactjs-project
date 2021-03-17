83366fdbc97bad8075cc5a8bad3aaf0391320bf3
LeandroKlug <leandro.softnetwork@gmail.com>
2021-03-17 15:25:05 -0300

    aula 1

--- project/src/App.js
+++ project/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: 'O titulo 1',
+        body: 'O corpo 1'
+      },
+      {
+        id: 2,
+        title: 'O titulo 2',
+        body: 'O corpo 2'
+      },
+      {
+        id: 3,
+        title: 'O titulo 3',
+        body: 'O corpo 3'
+      }
+    ]
+  };
+
+  render(){
+    const { posts } = this.state;
+
+    return (
+      <div className="App">
+        {posts.map(post => (
+          <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
